8f9da8cb68cbb26f1b1a5e179d62fcc89e98740d
unknown <kelvtmoni@gmail.com>
2025-12-10 03:54:38 +0000

    added iam policeis for eks cluster

--- infrastructure/eks/eks-accessentry.tf
+++ infrastructure/eks/eks-accessentry.tf
@@ -1,6 +1,17 @@
 resource "aws_eks_access_entry" "admin" {
   cluster_name      = aws_eks_cluster.studysync_cluster.name
-  principal_arn     = aws_iam_role.cluster.arn
-  kubernetes_groups = ["group-1", "group-2"]
+  principal_arn     = "arn:aws:iam::118162274891:user/tochi-admin"
+  kubernetes_groups = []
   type              = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "admin" {
+  cluster_name  = aws_eks_cluster.studysync_cluster.name
+  policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = "arn:aws:iam::118162274891:user/tochi-admin"
+
+  access_scope {
+    type       = "cluster"
+  }
+    depends_on = [aws_eks_access_entry.admin]
 }
